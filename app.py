351898cf68e5e7fb211baded8c3116f162a9b5bf
Jessica McElroy <mcelroyjessica@gmail.com>
2014-12-08 17:38:54 -0800

    fixing typo in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from app import app 
 import os
 
-PORT = int(os.environg.et("PORT", 5000))
+PORT = int(os.environ.get("PORT", 5000))
 
 if __name__== "__main__":
 	app.run(debug = True, host="0.0.0.0", port=PORT)
